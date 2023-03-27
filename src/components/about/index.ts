88a9b9fa371a5a001c5e5f38235d30072a4541f6
lizth93 <lizth93@hotmail.com>
2023-03-27 16:01:50 -0500

    About section is Added

---
+++ src/components/about/index.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import AboutMe from "./component";
+
+export default styled(AboutMe)`
+  background-color: #131516;
+  padding: 6.8rem 0 9.6rem 0;
+
+  .subheading {
+    display: block;
+    font-size: 1.7rem;
+
+    font-weight: 500;
+    color: #0c8599;
+    text-transform: uppercase;
+    margin-bottom: 1.6rem;
+    letter-spacing: 0.75px;
+  }
+  .heading-secondary {
+    color: #d8d4cf;
+  }
+  .objective-description {
+    font-size: 1.8rem;
+    line-height: 1.8;
+    color: #d8d4cf;
+  }
+`;
